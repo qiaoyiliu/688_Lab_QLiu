cf7a7c1f2bbcd2279604cdfe90692599498eb78f
Joy Liu <104224909+qiaoyiliu@users.noreply.github.com>
2024-09-12 16:59:38 +0000

    upload lab3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 lab1 = st.Page("streamlit_app_lab1.py", title="Lab 1", icon=":material/add_circle:")
 lab2 = st.Page("streamlit_app_lab2.py", title="Lab 2", icon=":material/add_circle:")
-pg = st.navigation([lab1, lab2])
+lab3 = st.Page("streamlit_app_lab3.py", title="Lab 3", icon=":material/add_circle:")
+pg = st.navigation([lab1, lab2, lab3])
 st.set_page_config(page_title="688Labs", page_icon=":material/edit:")
 pg.run()
