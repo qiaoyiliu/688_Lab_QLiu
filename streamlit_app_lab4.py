81166320efdd31f679a4e62641818167868f4ec2
Joy Liu <104224909+qiaoyiliu@users.noreply.github.com>
2024-09-19 16:20:59 +0000

    a

--- streamlit_app_lab4.py
+++ streamlit_app_lab4.py
@@ -9,73 +9,61 @@ import chromadb
 
 st.title("Joy's Lab4 question answering chatbot")
 
-openAI_model = st.sidebar.selectbox("Which model?",
-                                    ("mini", "regular"))
-
 topic = st.sidebar.selectbox("Topic", ("Generative AI", "Text Mining", "Data Science Overview"))
 
-if openAI_model == "mini":
-    model_to_use = "gpt-4o-mini"
-else:
-    model_to_use = "gpt_4o"
+#chroma_client = chromadb.PersistentClient(path="~/embeddings")
+chroma_client = chromadb.Client()
 
+def read_pdf(file):
+    file_name = file.name  
+    pdf_content = ""
+    with pdfplumber.open(file) as pdf:
+        for page in pdf.pages:
+            pdf_content += page.extract_text()
+        return file_name, pdf_content
+
+uploaded_file = st.file_uploader("Upload a document (.pdf)", type=("pdf"))
 
 if "client" not in st.session_state:
     openai_api_key = st.text_input("OpenAI API Key", type="password")
-    st.session_state.client = OpenAI(api_key=openai_api_key)
+    st.session_state.openai_client = OpenAI(api_key=openai_api_key)
 
+    if "Lab4_vectorDB" not in st.session_state:
+        st.session_state.Lab4_vectorDB = chroma_client.get_or_create_collection(name="Lab4Collection")
+        
+        def add_to_collection(collection, text, filename):
+            openai_client = st.session_state.client
+            response = openai_client.embeddings.create(
+                input=text,
+                model="text-embedding-3-small"
+            )
+            embedding = response.data[0].embedding
+            collection.add(
+                documents=[text],
+                ids=[filename],
+                embeddings=[embedding]
+            )
 
-client = chromadb.PersistentClient(path="/embeddings")
-chroma_client = chromadb.Client()
+        if uploaded_file is not None:
+            filename, text = read_pdf(uploaded_file)
+            
+            add_to_collection(st.session_state.Lab4_vectorDB, text, filename)
+            st.success(f"Document {filename} added to the vector DB.")
 
-if "Lab4_vectorDB" not in st.session_state:
-    st.session_state.Lab4_vectorDB = chroma_client.get_or_create_collection(name="Lab4Collection")
-    
-    def add_to_collection(collection, text, filename):
-        openai_client = st.session_state.client
-        response = openai_client.embeddings.create(
-            input=text,
-            model="text-embedding-3-small"
+        openai_client = st.session_state.openai_client
+        response = chroma_client.embeddings.create(
+        input=topic,
+        model="text-embedding-3-small"
         )
-        embedding = response.data[0].embedding
-        collection.add(
-            documents=[text],
-            ids=[filename],
-            embeddings=[embedding]
+        query_embedding = response.data[0].embedding
+        results = st.session_state.Lab4_vectorDB.query(
+            query_embedding=[query_embedding],
+            n_results=3
         )
-    
-    def read_pdf(file):
-        file_name = file.name  
-        pdf_content = ""
 
-        with pdfplumber.open(file) as pdf:
-            for page in pdf.pages:
-                pdf_content += page.extract_text()
+        for i in range(len(results['documents'][0])):
+            doc = results['documents'][0][i]
+            doc_id = results['ids'][0][i]
+            st.write(f"The following file/syllabus might be helpful: {doc_id}")
 
-        return file_name, pdf_content
 
-    uploaded_file = st.file_uploader("Upload a document (.pdf)", type=("pdf"))
-    
-    if uploaded_file is not None:
-        # If a file is uploaded, read the content and filename
-        filename, text = read_pdf(uploaded_file)
-
-        # Add the file to the vector database
-        add_to_collection(st.session_state.Lab4_vectorDB, text, filename)
-        st.success(f"Document {filename} added to the vector DB.")
-
-openai_client = st.session_state.client
-response = openai_client.embeddings.create(
-    input=topic,
-    model="text-embedding-3-small"
-)
-query_embedding = response.data[0].embedding
-results = st.session_state.Lab4_vectorDB.query(
-    query_embedding=[query_embedding],
-    n_results=3
-)
-
-for i in range(len(results['documents'][0])):
-    doc = results['documents'][0][i]
-    doc_id = results['ids'][0][i]
-    st.write(f"The following file/syllabus might be helpful: {doc_id}")
