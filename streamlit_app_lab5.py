a81746f663d384ca38a2c77f0ef9f9779ce2860f
Joy Liu <104224909+qiaoyiliu@users.noreply.github.com>
2024-09-26 14:54:31 +0000

    v3vwe

--- streamlit_app_lab5.py
+++ streamlit_app_lab5.py
@@ -4,3 +4,24 @@ import requests
 
 st.title("Joy's Lab5")
 
+def get_current_weather(location, API_key):
+    if "," in location:
+        location = location.split(",")[0].strip()
+    
+    urlbase = "https://api.openweathermap.org/data/2.5/"
+    urlweather = f"weather?q={location}&appid={"OPENWEATHER_API_KEY"}"
+    url = urlbase + urlweather
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract temperatures & Convert Kelvin to Celsius
+    temp = data['main']['temp'] - 273.15
+    feels_like = data['main']['feels_like'] - 273.15
+    temp_min = data['main']['temp_min'] - 273.15 temp_max = data['main']['temp_max'] - 273.15
+    humidity = data['main']['humidity']
+    return {"location": location,
+        "temperature": round(temp, 2),
+        "feels_like": round(feels_like, 2),
+        "temp_min": round(temp_min, 2), "temp_max": round(temp_max, 2),
+        "humidity": round(humidity, 2)
+    }
