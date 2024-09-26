100297d1ace38813b26db673a3876cbb594009e0
Joy Liu <104224909+qiaoyiliu@users.noreply.github.com>
2024-09-26 14:52:21 +0000

    ytfgtry

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ lab1 = st.Page("streamlit_app_lab1.py", title="Lab 1", icon=":material/add_circl
 lab2 = st.Page("streamlit_app_lab2.py", title="Lab 2", icon=":material/add_circle:")
 lab3 = st.Page("streamlit_app_lab3.py", title="Lab 3", icon=":material/add_circle:")
 lab4 = st.Page("streamlit_app_lab4.py", title="Lab 4", icon=":material/add_circle:")
+lab5 = st.Page("streamlit_app_lab5.py", title="Lab 5", icon=":material/add_circle:")
 
-pg = st.navigation([lab1, lab2, lab3, lab4])
+pg = st.navigation([lab1, lab2, lab3, lab4, lab5])
 st.set_page_config(page_title="688Labs", page_icon=":material/edit:")
 pg.run()
